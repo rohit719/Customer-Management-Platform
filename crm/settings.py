0acddae76b258112af9982870b4da2f83a9eebb8
Rohit Mehta <60610774+rohit719@users.noreply.github.com>
2020-08-19 01:06:19 +0530

    Add files via upload

--- crm/settings.py
+++ crm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tbd(u_il)35-1!4ci@&@)nbe#g)a_m-i)$k*=ab+hiey5oaj-t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['customer-management-platform.herokuapp.com','127.0.0.1']
 
@@ -80,23 +80,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo_1',
-        'USER':'rohit',
-        'PASSWORD':'realmadrid',
-        'HOST':'database-2.cpuyooufwkz3.us-east-2.rds.amazonaws.com',
-        'PORT':'5432'   
-    }
-}
-'''
+
 
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.sqlite3',
-        'Name':os.path.join(BASE_DIR,'db.sqlite3'),
+        'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
         }
     }
 
